57381dcb7e87ef27e88f0fadc7bb3db38957667f
Dawson Dohlen <d.dohlen@pm.me>
2025-01-20 20:47:13 -0800

    fix server setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
